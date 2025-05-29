7aa79a83e607099b1f1efefc6679b78d584dcd52
Fajri Rinaldi Chan <fajri@gariskode.com>
2025-05-29 14:15:14 +0700

    feat: migration & model

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,11 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'photo',
+        'position',
+        'linkedin',
+        'instagram',
+        'github',
         'email',
         'password',
     ];
